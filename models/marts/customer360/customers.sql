9b798b1f7d4e17cfe856d03607a6478548d87af3
NAME <dev@turntable.so>
2024-11-13 23:18:01 +0000

    wip

--- models/marts/customer360/customers.sql
+++ models/marts/customer360/customers.sql
@@ -1,62 +1,51 @@
 with
 
-customers as (
+    customers as (select * from {{ ref("stg_customers") }}),
 
-    select * from {{ ref('stg_customers') }}
+    orders_table as (select * from {{ ref("orders") }}),
 
-),
+    order_items_table as (select * from {{ ref("order_items") }}),
 
-orders_table as (
+    order_summary as (
 
-    select * from {{ ref('orders') }}
+        select
+            customer_id,
 
-),
+            count(distinct orders.order_id) as count_lifetime_orders,
+            count(distinct orders.order_id) > 1 as is_repeat_buyer,
+            min(orders.ordered_at) as first_ordered_at,
+            max(orders.ordered_at) as last_ordered_at,
+            sum(order_items.product_price) as lifetime_spend_pretax,
+            sum(orders.order_total) as lifetime_spend
 
-order_items_table as (
+        from orders_table as orders
 
-    select * from {{ ref('order_items') }}
-),
+        left join
+            order_items_table as order_items on orders.order_id = order_items.order_id
 
-order_summary as (
+        group by 1
 
-    select
-        customer_id,
+    ),
 
-        count(distinct orders.order_id) as count_lifetime_orders,
-        count(distinct orders.order_id) > 1 as is_repeat_buyer,
-        min(orders.ordered_at) as first_ordered_at,
-        max(orders.ordered_at) as last_ordered_at,
-        sum(order_items.product_price) as lifetime_spend_pretax,
-        sum(orders.order_total) as lifetime_spend
+    joined as (
 
-    from orders_table as orders
-    
-    left join order_items_table as order_items on orders.order_id = order_items.order_id
-    
-    group by 1
+        select
+            customers.*,
+            order_summary.count_lifetime_orders,
+            order_summary.first_ordered_at,
+            order_summary.last_ordered_at,
+            order_summary.lifetime_spend_pretax,
+            order_summary.lifetime_spend,
 
-),
+            case
+                when order_summary.is_repeat_buyer then 'returning' else 'new'
+            end as customer_type
 
-joined as (
+        from customers
 
-    select
-        customers.*,
-        order_summary.count_lifetime_orders,
-        order_summary.first_ordered_at,
-        order_summary.last_ordered_at,
-        order_summary.lifetime_spend_pretax,
-        order_summary.lifetime_spend,
+        left join order_summary on customers.customer_id = order_summary.customer_id
 
-        case
-            when order_summary.is_repeat_buyer then 'returning'
-            else 'new'
-        end as customer_type
+    )
 
-    from customers
-
-    left join order_summary
-        on customers.customer_id = order_summary.customer_id
-
-)
-
-select * from joined
+select *
+from joined
