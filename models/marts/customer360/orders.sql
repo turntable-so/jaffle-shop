d6ac0141204d7a6183d7510ff8fd7cf1d56a5a72
NAME <dev@turntable.so>
2024-12-03 20:23:08 +0000

    wip

--- models/marts/customer360/orders.sql
+++ models/marts/customer360/orders.sql
@@ -1,46 +1,35 @@
-with 
---test
-orders as (
-    
-    select * from {{ ref('stg_orders')}}
+-- test
+with
+    orders as (select * from {{ ref("stg_orders") }}),
 
-),
+    order_items_table as (select * from {{ ref("order_items") }}),
 
-order_items_table as (
-    
-    select * from {{ ref('order_items')}}
+    order_items_summary as (
 
-),
+        select
+            order_items.order_id,
+            sum(supply_cost) as order_cost,
+            sum(is_food_item) as count_food_items,
+            sum(is_drink_item) as count_drink_items
 
-order_items_summary as (
+        from order_items_table as order_items
 
-    select
+        group by 1
 
-        order_items.order_id,
+    ),
 
-        sum(supply_cost) as order_cost,
-        sum(is_food_item) as count_food_items,
-        sum(is_drink_item) as count_drink_items
+    compute_booleans as (
+        select
 
+            orders.*,
+            count_food_items > 0 as is_food_order,
+            count_drink_items > 0 as is_drink_order,
+            order_cost
 
-    from order_items_table as order_items
+        from orders
 
-    group by 1
+        left join order_items_summary on orders.order_id = order_items_summary.order_id
+    )
 
-),
-
-
-compute_booleans as (
-    select
-
-        orders.*,
-        count_food_items > 0 as is_food_order,
-        count_drink_items > 0 as is_drink_order,
-        order_cost
-
-    from orders
-    
-    left join order_items_summary on orders.order_id = order_items_summary.order_id
-)
-
-select * from compute_booleans
+select *
+from compute_booleans
