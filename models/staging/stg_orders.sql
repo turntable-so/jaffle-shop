0f29db2337cf857befb3101d32a8a38e58d47c58
NAME <dev@turntable.so>
2024-12-09 17:06:47 +0000

    wip

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,31 +1,36 @@
 with
 
-source as (
+    source as (
 
-    select * from {{ source('ecom', 'raw_orders') }}
-    -- if you generate a larger dataset, you can limit the timespan to the current time with the following line
+        select * from {{ source("ecom", "raw_orders") }}
+    -- if you generate a larger dataset, you can limit the timespan to the current
+    -- time with the following line
     -- where ordered_at <= {{ var('truncate_timespan_to') }}
+    ),
 
-),
+    renamed as (
 
-renamed as (
+        select
 
-    select
+            -- --------  ids
+            id as order_id,
+            store_id as location_id,
+            customer as customer_id,
 
-        ----------  ids
-        id as order_id,
-        store_id as location_id,
-        customer as customer_id,
+            -- -------- numerics
+            (order_total / 100.0) as order_total,
+            (tax_paid / 100.0) as tax_paid,
 
-        ---------- numerics
-        (order_total / 100.0) as order_total,
-        (tax_paid / 100.0) as tax_paid,
+            -- -------- timestamps
+            {{ dbt.date_trunc("day", "ordered_at") }} as ordered_at
 
-        ---------- timestamps
-        {{dbt.date_trunc('day','ordered_at')}} as ordered_at
+        from source
 
-    from source
+    ),
 
-)
+    deduped as (
+        select distinct on (order_id) * from renamed order by order_id, ordered_at desc
+    )
 
-select * from renamed
+select *
+from deduped
