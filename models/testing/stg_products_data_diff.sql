e5a8cd88f7d2b5fe66bc4f6fe968456f5c637c25
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2025-01-05 17:41:28 -0600

    add data diff test model

---
+++ models/testing/stg_products_data_diff.sql
@@ -0,0 +1,20 @@
+select 
+    product_id,
+    product_name,
+    product_type,
+    product_description,
+    case when product_id = 'JAF-001' then product_price else product_price * 1.1 end as product_price
+
+from {{ ref('stg_products') }}
+where is_food_item = 1
+
+union all
+
+-- Add one new row as a test
+select 
+    '999' as product_id,
+    'Manual Test Product' as product_name,
+    'Test Type' as product_type,
+    'A manually added test product' as product_description,
+    11.00 as product_price
+
